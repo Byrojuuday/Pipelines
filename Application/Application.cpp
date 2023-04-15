503c9a333ef0976395d970c7fa969e281bed7733
Uday Kumar Byroju <udaykumarbyroju@udays-air.wifi.uc.edu>
2023-04-14 20:24:59 -0400

    Adding cout changes to checck builds

--- Application/Application.cpp
+++ Application/Application.cpp
@@ -4,7 +4,6 @@
 #include "framework.h"
 #include "Application.h"
 
-
 CoolClass::CoolClass(void)
 {
 }
@@ -13,11 +12,11 @@ int CoolClass::Calc(int a, int b)
 {
     if (a == b)
     {
+        std::cout << "Just Printing to check the workflows a is equal to b Team 7" << std::endl;
         return 2 * a;
     }
     else
     {
         return a + b;
     }
-    
 }
